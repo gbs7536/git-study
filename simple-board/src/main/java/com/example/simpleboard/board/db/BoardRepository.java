cb7412608094d10ee6638f4d5df857a806a2203a
User <gbs7536@naver.com>
2025-03-02 22:19:00 +0900

    간단한 게시판 0302

---
+++ simple-board/src/main/java/com/example/simpleboard/board/db/BoardRepository.java
@@ -0,0 +1,7 @@
+// BoardRepository
+package com.example.simpleboard.board.db;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+}
